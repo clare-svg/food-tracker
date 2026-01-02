f7187e1a8dc8c9603e0dbb0b2248d8ded5eaca64
clare-svg <clarehaycock89@gmail.com>
2026-01-02 11:07:59 +0000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = "food-tracker-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE ? caches.delete(k) : null)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
